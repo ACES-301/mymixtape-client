0f8c9ceb4cf3b96d4dc8347116ba35545afe0b1a
Stacy Yu <stacyyuu@gmail.com>
2022-11-17 15:07:07 -0800

    final

--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,7 @@ class Login extends React.Component {
     render() {
         return (
             <div className="login">
-            <img className="cassette" src={Cassette}/>
+            <img className="cassette" src={Cassette} alt={'cassette'}/>
             <h1>Login to Discover New Music</h1>
             <LoginButton />
             </div>
